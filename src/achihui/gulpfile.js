85dfa327b804dabe61ee1366590dee821e5e3e7f
Hongjun Qian <hoq@microsoft.com>
2016-10-09 13:14:57 +0800

    Start move to angular2

---
+++ src/achihui/gulpfile.js
@@ -0,0 +1,14 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+gulp.task('setup-libs', function () {
+    // place code for your default task here
+});
+
+gulp.task('setup-libs', function () {
+    // place code for your default task here
+});
--- src/achihui/npminstall.cmd
+++
@@ -1,3 +0,0 @@
-cd wwwroot
-npm install
-cd ..
