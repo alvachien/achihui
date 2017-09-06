14021666d334097831c2a31254176fbc1266de59
Alva Chien <alvachien@163.com>
2017-09-06 14:55:29 +0800

    Prepare for the refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
-import { AuthService } from './services/auth.service';
-import { UIStatusService } from './services/uistatus.service';
 import { environment } from '../environments/environment';
-import { DomSanitizer } from '@angular/platform-browser';
-import { TranslateService } from '@ngx-translate/core';
-import { MdIconRegistry } from '@angular/material';
-import { TdLoadingService, LoadingType } from '@covalent/core';
-
 
 @Component({
   selector: 'hih-root',
@@ -16,56 +9,7 @@ import { TdLoadingService, LoadingType } from '@covalent/core';
 export class AppComponent implements OnInit {
   //private isAuthInitial: boolean;
 
-  constructor(private _authService: AuthService,
-    private _uistatus: UIStatusService,
-    private _translateService: TranslateService,
-    private _loadingService: TdLoadingService,
-    private _iconRegistry: MdIconRegistry,
-    private _domSanitizer: DomSanitizer,
-    viewContainerRef: ViewContainerRef) {
-    if (environment.DebugLogging) {
-      console.log("ACHIHUI Log: Entering constructor of AppComponent");
-    }
-
-    let arlang: string[] = [];
-    for(let ap of this._uistatus.arLang)
-      arlang.push(ap.IsoName);
-    this._translateService.addLangs(arlang);
-    this._translateService.setDefaultLang(this._uistatus.curLang ? this._uistatus.curLang : "en");
-
-    this._uistatus.obsCurLanguage.subscribe(x => {
-      if (environment.DebugLogging) {
-        console.log("ACHIHUI Log: Entering current language change in AppComponent: " + x);
-      }
-      if (x) {
-        this._translateService.use(x);
-      }      
-    }, error => {
-    }, () => {
-    });
-
-    this._authService.authContent.subscribe(x => {
-      // if (this.isAuthInitial) {
-      //   this.isAuthInitial = false;
-      // } else {
-        if (environment.DebugLogging) {
-          console.log("ACHIHUI Log: Entering subscribe of authContent in AppComponent");
-        }
-
-        this._uistatus.setIsLogin(x.isAuthorized);
-      //}
-    });
-
-    this._iconRegistry.addSvgIconInNamespace('assets', 'github',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'angular',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/angular.ico'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'covalent',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/covalent.svg'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'covalent-mark',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/covalent-mark.svg'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'hihlogo',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/hihapplogo.svg'));
+  constructor(viewContainerRef: ViewContainerRef) {
   }
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpModule, Http } from '@angular/http';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 
-import { UIRefModule } from './uiref.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { AboutComponent } from './about/about.component';
-import { UserdetailComponent } from './userdetail/userdetail.component';
-import { AuthService } from './services/auth.service';
-import { UIStatusService } from './services/uistatus.service';
-import { BufferService } from './services/buff.service';
-import { AppRoutingModule } from './app-routing.module';
-import { TranslateService } from '@ngx-translate/core';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function funcHttpLoaderFactory(http: Http) {
-    return new TranslateHttpLoader(http, '/assets/locales/', '.json');
-}
 
 @NgModule({
   imports: [
@@ -35,30 +18,12 @@ export function funcHttpLoaderFactory(http: Http) {
     ReactiveFormsModule,
     HttpModule,
     MaterialModule,
-    UIRefModule,
-    FlexLayoutModule,
-    AppRoutingModule,
-    NgxChartsModule,
-    TranslateModule.forRoot({
-        loader: {
-            provide: TranslateLoader,
-            useFactory: funcHttpLoaderFactory,
-            deps: [Http]
-        }
-    })
+    NgxChartsModule
   ],
   declarations: [
     AppComponent,
-    HomeComponent,
-    PagenotfoundComponent,
-    AboutComponent,
-    UserdetailComponent
   ],
   providers: [
-    AuthService,
-    UIStatusService,
-    TranslateService,
-    BufferService
   ],
   bootstrap: [AppComponent]
 })
