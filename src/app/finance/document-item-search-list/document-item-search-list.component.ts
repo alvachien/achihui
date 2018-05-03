ac0aa1f647cbc928e84a37a2097459a9216a0293
Alva Chien <alvachien@163.com>
2018-05-03 22:42:15 +0800

    Add new codes

--- src/app/finance/document-item-search-list/document-item-search-list.component.ts
+++ src/app/finance/document-item-search-list/document-item-search-list.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { GeneralFilterOperatorEnum, GeneralFilterItem, UIDisplayString, UIDisplayStringUtil } from '../../model';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { GeneralFilterOperatorEnum, GeneralFilterItem, UIDisplayString, UIDisplayStringUtil, DocumentItem } from '../../model';
 import { Observable, forkJoin, merge, of } from 'rxjs';
 import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 import { HttpParams, HttpClient, HttpHeaders, HttpResponse, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '../../services/auth.service';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'hih-fin-document-item-search-list',
   templateUrl: './document-item-search-list.component.html',
   styleUrls: ['./document-item-search-list.component.scss'],
 })
-export class DocumentItemSearchListComponent implements OnInit {
+export class DocumentItemSearchListComponent implements OnInit, AfterViewInit {
   filters: GeneralFilterItem[] = [];
   allOperators: UIDisplayString[] = [];
   allFields: any[] = [];
   filterEditable: boolean = true;
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource: any = new MatTableDataSource<DocumentItem>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private _http: HttpClient,
     private _authService: AuthService) {
@@ -42,6 +46,14 @@ export class DocumentItemSearchListComponent implements OnInit {
 
   ngOnInit(): void {
     this.onAddFilter();
+  }
+
+  /**
+   * Set the paginator after the view init since this component will
+   * be able to query its view for the initialized paginator.
+   */
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
   public onAddFilter(): void {
