dc190a65654ff93865f5c2afc29f261207eb5c3a
Alva Chien <alvachien@163.com>
2018-09-18 18:54:00 +0800

    Try to solve ExpressionChangedAfterItHasBeenCheckedError in ADP doc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment: any = {
   production: false,
   LoginRequired: true,
-  CurrentVersion: '0.5.10',
+  CurrentVersion: '0.5.11',
   ReleasedDate: '2018.10.1',
 
   DebugLogging: true,
