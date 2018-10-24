02b71fb0895d54c5820c56d917f1fcf12b494ded
Alva Chien <alvachien@163.com>
2018-10-24 22:57:00 +0800

    Add new codes

--- src/app/finance/report/report.component.ts
+++ src/app/finance/report/report.component.ts
@@ -121,6 +121,50 @@ export class ReportComponent implements OnInit, AfterViewInit, OnDestroy {
   selectedMOMScope: OverviewScopeEnum;
   momExcludeTransfer: boolean;
   momScopes: UIDisplayString[];
+  optionMoM: any = {
+      // color: ['#3F98DB'],
+      tooltip : {
+          trigger: 'axis',
+          axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+              type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+          }
+      },
+      grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+      },
+      xAxis : [
+          {
+              type : 'category',
+              data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              axisTick: {
+                  alignWithLabel: true
+              }
+          }
+      ],
+      yAxis : [
+          {
+              type : 'value',
+              splitArea: {show: true}
+          }
+      ],
+      series : [
+          {
+              //name:'直接访问',
+              type:'bar',
+              //barWidth: '60%',
+              data:[10, 52, 200, 334, 390, 330, 220]
+          },
+          {
+              //name:'直接访问',
+              type:'bar',
+              //barWidth: '60%',
+              data:[10, 52, 200, 334, 390, 330, 220]
+          }
+      ]
+  };
 
   // B.S.
   @ViewChild('chartAcntCtgy') chartAcntCtgy: ElementRef;
