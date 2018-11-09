26a3be377bf023ab04d995351d07d761f82c3953
Alva Chien <alvachien@163.com>
2018-11-09 17:52:26 +0800

    Prepare refactor DocumentDetail

--- src/app/finance/document-detail/document-detail.component.ts
+++ src/app/finance/document-detail/document-detail.component.ts
@@ -1,8 +1,5 @@
-import {
-  Component, OnInit, OnDestroy, AfterViewInit, EventEmitter,
-  Input, Output, ViewContainerRef,
-} from '@angular/core';
-import { DataSource } from '@angular/cdk/collections';
+import { Component, OnInit, OnDestroy, AfterViewInit, EventEmitter } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialog, MatSnackBar, MatTableDataSource } from '@angular/material';
 import { Observable, forkJoin, Subject, BehaviorSubject, merge, of } from 'rxjs';
@@ -15,41 +12,23 @@ import { HomeDefDetailService, FinanceStorageService, FinCurrencyService, UIStat
 import { MessageDialogButtonEnum, MessageDialogInfo, MessageDialogComponent } from '../../message-dialog';
 import { ENTER, COMMA, } from '@angular/cdk/keycodes';
 
-/**
- * Data source of Document Items
- */
-export class DocumentItemDataSource extends DataSource<any> {
-  constructor(private _parentComponent: DocumentDetailComponent) {
-    super();
-  }
-
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<DocumentItem[]> {
-    const displayDataChanges: any[] = [
-      this._parentComponent.itemOperEvent,
-    ];
-
-    return merge(...displayDataChanges).pipe(map(() => {
-      return this._parentComponent.detailObject.Items;
-    }));
-  }
-
-  disconnect(): void {
-    // Empty
-  }
-}
-
 @Component({
   selector: 'hih-finance-document-detail',
   templateUrl: './document-detail.component.html',
   styleUrls: ['./document-detail.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class DocumentDetailComponent implements OnInit {
   private routerID: number = -1; // Current object ID in routing
   public currentMode: string;
   public detailObject: Document | undefined = undefined;
-  public uiMode: UIMode = UIMode.Create;
-  public step: number = 0;
+  public uiMode: UIMode = UIMode.Display;
   public arUIAccount: UIAccountForSelection[] = [];
   public uiAccountStatusFilter: string | undefined;
   public uiAccountCtgyFilter: IAccountCategoryFilter | undefined;
@@ -58,8 +37,8 @@ export class DocumentDetailComponent implements OnInit {
   // Enter, comma
   separatorKeysCodes: any[] = [ENTER, COMMA];
 
+  dataSource: MatTableDataSource<DocumentItem> = new MatTableDataSource<DocumentItem>();
   displayedColumns: string[] = ['ItemId', 'AccountId', 'TranType', 'Amount', 'Desp', 'ControlCenter', 'Order'];
-  dataSource: DocumentItemDataSource | undefined;
   itemOperEvent: EventEmitter<undefined> = new EventEmitter<undefined>(undefined);
 
   get isFieldChangable(): boolean {
@@ -82,7 +61,6 @@ export class DocumentDetailComponent implements OnInit {
     public _storageService: FinanceStorageService,
     public _currService: FinCurrencyService) {
     this.detailObject = new Document();
-    this.dataSource = new DocumentItemDataSource(this);
   }
 
   ngOnInit(): void {
@@ -114,11 +92,12 @@ export class DocumentDetailComponent implements OnInit {
       this._activateRoute.url.subscribe((x: any) => {
         if (x instanceof Array && x.length > 0) {
           if (x[0].path === 'create') {
-            this.onInitCreateMode();
+            // Create is not allowed!
+            // this.onInitCreateMode();
           } else if (x[0].path === 'edit') {
             this.routerID = +x[1].path;
 
-            this.uiMode = UIMode.Change;
+            this.uiMode = UIMode.Display;
           } else if (x[0].path === 'display') {
             this.routerID = +x[1].path;
 
@@ -171,163 +150,7 @@ export class DocumentDetailComponent implements OnInit {
     });
   }
 
-  public setStep(index: number): void {
-    this.step = index;
-  }
-
-  public nextStep(): void {
-    this.step++;
-  }
-
-  public prevStep(): void {
-    this.step--;
-  }
-
-  public canSubmit(): boolean {
-    if (!this.isFieldChangable) {
-      return false;
-    }
-
-    // Check name
-    if (this.detailObject.Desp.trim().length <= 0) {
-      return false;
-    }
-
-    return true;
-  }
-
-  public onCreateDocItem(): void {
-    let di: DocumentItem = new DocumentItem();
-    di.ItemId = this.getNextItemID();
-    this.detailObject.Items.push(di);
-    this.itemOperEvent.emit();
-  }
-
-  public onDeleteDocItem(di: any): void {
-    let idx: number = 0;
-    for (let i: number = 0; i < this.detailObject.Items.length; i ++) {
-      if (this.detailObject.Items[i].ItemId === di.ItemId) {
-        idx = i;
-        break;
-      }
-    }
-
-    this.detailObject.Items.splice(idx);
-    this.itemOperEvent.emit();
-  }
-
-  public onSubmit(): void {
-    if (this.uiMode === UIMode.Create) {
-      // Check!
-      if (!this.detailObject.onVerify({
-        ControlCenters: this._storageService.ControlCenters,
-        Orders: this._storageService.Orders,
-        Accounts: this._storageService.Accounts,
-        DocumentTypes: this._storageService.DocumentTypes,
-        TransactionTypes: this._storageService.TranTypes,
-        Currencies: this._currService.Currencies,
-        BaseCurrency: this._homedefService.ChosedHome.BaseCurrency,
-      })) {
-        // Show a dialog for error details
-        const dlginfo: MessageDialogInfo = {
-          Header: this._uiStatusService.getUILabel(UICommonLabelEnum.Error),
-          ContentTable: this.detailObject.VerifiedMsgs,
-          Button: MessageDialogButtonEnum.onlyok,
-        };
-
-        this._dialog.open(MessageDialogComponent, {
-          disableClose: false,
-          width: '500px',
-          data: dlginfo,
-        });
-
-        return;
-      }
-
-      this._storageService.createDocumentEvent.subscribe((x: any) => {
-        if (environment.LoggingLevel >= LogLevel.Debug) {
-          console.log(`AC_HIH_UI [Debug]: Receiving createDocumentEvent in DocumentDetailComponent with : ${x}`);
-        }
-
-        // Navigate back to list view
-        if (x instanceof Document) {
-          let snackbarRef: any = this._snackbar.open(this._uiStatusService.getUILabel(UICommonLabelEnum.DocumentPosted),
-            this._uiStatusService.getUILabel(UICommonLabelEnum.CreateAnotherOne), {
-            duration: 3000,
-          });
-
-          let recreate: boolean = false;
-          snackbarRef.onAction().subscribe(() => {
-            recreate = true;
-
-            this.onInitCreateMode();
-            this.setStep(0);
-            this.itemOperEvent.emit();
-            // this._router.navigate(['/finance/document/create/']);
-          });
-
-          snackbarRef.afterDismissed().subscribe(() => {
-            // Navigate to display
-            if (!recreate) {
-              this._router.navigate(['/finance/document/display/' + x.Id.toString()]);
-            }
-          });
-        } else {
-          // Show error message
-          const dlginfo: MessageDialogInfo = {
-            Header: this._uiStatusService.getUILabel(UICommonLabelEnum.Error),
-            Content: x.toString(),
-            Button: MessageDialogButtonEnum.onlyok,
-          };
-
-          this._dialog.open(MessageDialogComponent, {
-            disableClose: false,
-            width: '500px',
-            data: dlginfo,
-          }).afterClosed().subscribe((x2: any) => {
-            // Do nothing!
-            if (environment.LoggingLevel >= LogLevel.Debug) {
-              console.log(`AC_HIH_UI [Debug]: Message dialog result ${x2}`);
-            }
-          });
-        }
-      });
-
-      this.detailObject.HID = this._homedefService.ChosedHome.ID;
-      this._storageService.createDocument(this.detailObject);
-    }
-  }
-
   public onBackToList(): void {
     this._router.navigate(['/finance/document/']);
-  }
-
-  private getNextItemID(): number {
-    if (this.detailObject.Items.length <= 0) {
-      return 1;
-    }
-
-    let nMax: number = 0;
-    for (let item of this.detailObject.Items) {
-      if (item.ItemId > nMax) {
-        nMax = item.ItemId;
-      }
-    }
-
-    return nMax + 1;
-  }
-
-  private onInitCreateMode(): void {
-    this.detailObject = new Document();
-    this.uiMode = UIMode.Create;
-    this.uiAccountStatusFilter = 'Normal';
-    this.uiAccountCtgyFilter = {
-      skipADP: true,
-      skipLoan: true,
-      skipAsset: true,
-    };
-    this.uiOrderFilter = true;
-    this.detailObject.HID = this._homedefService.ChosedHome.ID;
-    this.detailObject.TranCurr = this._homedefService.ChosedHome.BaseCurrency;
   }
 }
