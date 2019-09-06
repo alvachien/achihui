ece809481c1208f0a351703124f96cd86a5fc83c
Alva Chien <alvachien@163.com>
2019-09-06 08:54:11 +0800

    Continues the development of AntDesign

--- Elders/layout-header/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './fullscreen.component';
-export * from './search.component';
-export * from './user.component';
-export * from './task.component';
-export * from './task.component';
-export * from './layout-header.component';
-
--- Elders/layout-sidebar/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './layout-sidebar.component';
