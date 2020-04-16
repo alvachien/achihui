55539688a2765bb510243c21130a5b9565d1b2ce
Alva Chien <alvachien@163.com>
2020-04-17 00:24:28 +0800

    Add new codes

--- src/app/pages/reusable-components/reusable-components.module.ts
+++ src/app/pages/reusable-components/reusable-components.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
 import { MarkdownEditorComponent } from './markdown-editor/markdown-editor.component';
 
 @NgModule({
@@ -10,6 +13,8 @@ import { MarkdownEditorComponent } from './markdown-editor/markdown-editor.compo
   imports: [
     CommonModule,
     FormsModule,
+    NzButtonModule,
+    NzIconModule,
   ],
   exports: [
     MarkdownEditorComponent,
