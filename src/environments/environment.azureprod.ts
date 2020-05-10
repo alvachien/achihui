1f678340fab6c341f2ed0d6684d083bc8e0ecb4e
Alva Chien <alvachien@163.com>
2020-05-10 19:22:36 +0800

    Bump the version

--- src/environments/environment.azureprod.ts
+++ src/environments/environment.azureprod.ts
@@ -1,7 +1,7 @@
 export const environment: any = {
   production: true,
   LoginRequired: true,
-  CurrentVersion: '1.1.98',
+  CurrentVersion: '1.1.100',
   ReleasedDate: '2020.9.1',
   DefaultLanguage: 'en',
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
 
   LoginRequired: true,
-  CurrentVersion: '1.1.98',
+  CurrentVersion: '1.1.100',
   ReleasedDate: '2020.9.1',
   DefaultLanguage: 'zh',
 
