78ce410ddb5fcd895980cfac6055528c2c8e4265
Alva Chien <alvachien@163.com>
2020-10-18 20:21:40 +0800

    Pass the build for AliCloud

--- src/app/pages/user-detail/user-detail.module.ts
+++ src/app/pages/user-detail/user-detail.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslocoModule } from '@ngneat/transloco';
 import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 import { UserDetailRoutingModule } from './user-detail-routing.module';
 import { UserDetailComponent } from './user-detail';
@@ -14,11 +17,16 @@ import { UserDetailComponent } from './user-detail';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzPageHeaderModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzDescriptionsModule,
+    NzSwitchModule,
+
     UserDetailRoutingModule,
     TranslocoModule,
-    NzPageHeaderModule,
-    NzSwitchModule,
-    NzDescriptionsModule,
   ]
 })
 export class UserDetailModule { }
