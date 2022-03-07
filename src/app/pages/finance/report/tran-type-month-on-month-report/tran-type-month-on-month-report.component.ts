edd48a9dcc3ccb5158b2532ff79d8bd778bfcb6e
Alva Chien <alvachien@163.com>
2022-03-07 15:43:30 +0800

    Prepare for the updgrade

--- src/app/pages/finance/report/tran-type-month-on-month-report/tran-type-month-on-month-report.component.ts
+++ src/app/pages/finance/report/tran-type-month-on-month-report/tran-type-month-on-month-report.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { translate } from '@ngneat/transloco';
+import { EChartsOption } from 'echarts';
 import { NzCascaderOption } from 'ng-zorro-antd/cascader';
+import { NzModalService } from 'ng-zorro-antd/modal';
 import { lastValueFrom } from 'rxjs';
 
 import { FinanceOdataService } from 'src/app/services';
@@ -13,7 +16,8 @@ import { LogLevel, ModelUtility, ConsoleLogTypeEnum, UIDisplayStringUtil,
 })
 export class TranTypeMonthOnMonthReportComponent implements OnInit {
 
-  constructor(private oDataService: FinanceOdataService) {
+  constructor(private oDataService: FinanceOdataService,
+    private modalService: NzModalService) {
     ModelUtility.writeConsoleLog('AC_HIH_UI [Debug]: Entering TranTypeMonthOnMonthReportComponent constructor...',
       ConsoleLogTypeEnum.debug);
   }
@@ -21,6 +25,8 @@ export class TranTypeMonthOnMonthReportComponent implements OnInit {
   availableTranTypes: NzCascaderOption[] = [];
   selectedTranTypes: number[] | null = null;
   selectedPeriod = '3';
+  chartOption: EChartsOption | null = null;
+
   get isGoButtonDisabled(): boolean {
     if (this.selectedTranTypes === null) {
       return true;
@@ -77,7 +83,14 @@ export class TranTypeMonthOnMonthReportComponent implements OnInit {
           });
         },
         error: err => {
-          // Error
+          ModelUtility.writeConsoleLog(`AC_HIH_UI [Error]: Entering TranTypeMonthOnMonthReportComponent ngOnInit fetchAllTranTypes failed ${err}`,
+            ConsoleLogTypeEnum.error);
+
+          this.modalService.error({
+            nzTitle: translate('Common.Error'),
+            nzContent: err,
+            nzClosable: true,
+          });
         }
       });
   }
@@ -87,6 +100,8 @@ export class TranTypeMonthOnMonthReportComponent implements OnInit {
   }
 
   refreshData(): void {
+    ModelUtility.writeConsoleLog(`AC_HIH_UI [Debug]: Entering TranTypeMonthOnMonthReportComponent refreshData`,
+      ConsoleLogTypeEnum.debug);
     if (this.selectedTranTypes === null || this.selectedTranTypes?.length <= 0) {
       return;
     }
@@ -95,12 +110,94 @@ export class TranTypeMonthOnMonthReportComponent implements OnInit {
     this.oDataService.fetchReportByTransactionTypeMoM(trantype, this.selectedPeriod).subscribe({
       next: val => {
         // Fetch out data
+        this.chartOption = {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              crossStyle: {
+                color: '#999'
+              }
+            }
+          },
+          toolbox: {
+            feature: {
+              dataView: { show: true, readOnly: false },
+              magicType: { show: true, type: ['line', 'bar'] },
+              restore: { show: true },
+              saveAsImage: { show: true }
+            }
+          },
+          legend: {
+            data: ['Evaporation', 'Precipitation', 'Temperature']
+          },
+          xAxis: [
+            {
+              type: 'category',
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              axisPointer: {
+                type: 'shadow'
+              }
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value',
+              name: 'Precipitation',
+              min: 0,
+              max: 250,
+              interval: 50,
+              axisLabel: {
+                formatter: '{value} ml'
+              }
+            },
+            {
+              type: 'value',
+              name: 'Temperature',
+              min: 0,
+              max: 25,
+              interval: 5,
+              axisLabel: {
+                formatter: '{value} °C'
+              }
+            }
+          ],
+          series: [
+            {
+              name: 'Evaporation',
+              type: 'bar',
+              data: [
+                2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
+              ]
+            },
+            {
+              name: 'Precipitation',
+              type: 'bar',
+              data: [
+                2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
+              ]
+            },
+            {
+              name: 'Temperature',
+              type: 'line',
+              yAxisIndex: 1,
+              data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+            }
+          ]
+        };
       },
       error: err => {
+        ModelUtility.writeConsoleLog(`AC_HIH_UI [Error]: Entering TranTypeMonthOnMonthReportComponent refreshData failed ${err}`,
+          ConsoleLogTypeEnum.error);
+
+        this.modalService.error({
+          nzTitle: translate('Common.Error'),
+          nzContent: err,
+          nzClosable: true,
+        });
 
       }
     });
-
   }
 
   // handleAreaClick(e: Event, label: string, option: NzCascaderOption): void {
