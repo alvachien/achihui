ac1d7d4cfc9b1e4cbc16acf7c0e329c5c4ee1a26
Alva Chien <alvachien@163.com>
2022-11-06 21:19:18 +0800

    Add new codes

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,11 +1,37 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { OidcSecurityService, PublicEventsService } from 'angular-auth-oidc-client';
+import { of } from 'rxjs';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
+  let securService: any;
+  let eventService: any;
+  let checkAuthSpy: any;
+  let registerForEventsSpy: any;
+
+  beforeAll(() => {
+    securService = jasmine.createSpyObj('OidcSecurityService', [
+      'checkAuth',
+    ]);
+    checkAuthSpy = securService.checkAuth.and.returnValue(of({}));
+ 
+    eventService = jasmine.createSpyObj('PublicEventsService', [
+      'registerForEvents',
+    ]);  
+    registerForEventsSpy = eventService.registerForEvents.and.returnValue(of({}));
+  });
+
   beforeEach(() => {
+    const authServiceStub: Partial<OidcSecurityService> = {};
+    const eventServiceStub: Partial<PublicEventsService> = {};
+
     TestBed.configureTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: OidcSecurityService, useValue: authServiceStub },
+        { provide: PublicEventsService, useValue: eventServiceStub },
+      ],
     });
   });
 
